5d612631192f201ce43d24a43a7bd09e47510add
Geoffrey Liu <liug.2012@gmail.com>
2014-12-19 22:24:52 -0800

    fix bad formatting in header

--- header.php
+++ header.php
@@ -32,21 +32,21 @@
 	<header id="masthead" class="site-header" role="banner">
 		<?php if ( get_theme_mod( 'themonic_logo' ) ) : ?>
 		
-		<div class="themonic-logo">
-        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo get_theme_mod( 'themonic_logo' ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" /></a>
-		</div>
-	<?php if( get_theme_mod( 'iconic_one_social_activate' ) == '1') {
-		the_social_media();
-	} ?>
+			<div class="themonic-logo">
+	        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo get_theme_mod( 'themonic_logo' ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" /></a>
+			</div>
+			<?php if( get_theme_mod( 'iconic_one_social_activate' ) == '1') {
+				the_social_media();
+			} ?>
 
 		<?php else : ?>
-		<div class="hgroup">
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-				<br /> <a class="site-description"><?php bloginfo( 'description' ); ?></a>
-		</div>
-	<?php if( get_theme_mod( 'iconic_one_social_activate' ) == '1') {
-		the_social_media();
-	} ?>	
+			<div class="hgroup">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+					<br /> <a class="site-description"><?php bloginfo( 'description' ); ?></a>
+			</div>
+			<?php if ( get_theme_mod( 'iconic_one_social_activate' ) == '1' ) :
+				the_social_media();
+			endif; ?>
 		<?php endif; ?>
 
 		<nav id="site-navigation" class="themonic-nav" role="navigation">
@@ -85,42 +85,50 @@ function the_social_media() {
 				$ICON_DIM = "width='$ICON_SIZE'";
 			?>
 			
-			<?php if(!ctype_space($tw) && strlen($tw) > 0) : ?>
-			<!-- twitter -->
-			<a href="<?php echo $tw; ?>" target="_blank"><span class="genericon genericon-twitter"></span></a>
+			<?php if ( ! ctype_space( $tw ) && strlen( $tw ) > 0 ) : ?>
+				<!-- twitter -->
+				<a href="<?php echo $tw; ?>" target="_blank"><span class="genericon genericon-twitter"></span></a>
 			<!--<a class="webicon twitter" href="<?php echo $tw; ?>" target="_BLANK">Twitter</a>-->
 			<?php endif; ?>
-			<?php if(!ctype_space($fb) && strlen($fb) > 0) : ?>
-			<!-- facebook -->
-			<a href="<?php echo $fb; ?>" target="_blank"><span class="genericon genericon-facebook"></span></a>
+			
+			<?php if ( ! ctype_space( $fb ) && strlen( $fb ) > 0 ) : ?>
+				<!-- facebook -->
+				<a href="<?php echo $fb; ?>" target="_blank"><span class="genericon genericon-facebook"></span></a>
 			<?php endif; ?>
-			<?php if(!ctype_space($yt) && strlen($yt) > 0) : ?>
-			<!-- youtube -->
-			<a href="<?php echo $yt; ?>" target="_blank"><span class="genericon genericon-youtube"></span></a>
+			
+			<?php if ( ! ctype_space( $yt ) && strlen( $yt ) > 0 ) : ?>
+				<!-- youtube -->
+				<a href="<?php echo $yt; ?>" target="_blank"><span class="genericon genericon-youtube"></span></a>
 			<?php endif; ?>
-			<?php if(!ctype_space($gp) && strlen($gp) > 0) : ?>
-			<!-- google+ -->
-			<a href="<?php echo $gp; ?>" rel="author" target="_blank"><span class="genericon genericon-googleplus-alt"></span></a>
+			
+			<?php if ( ! ctype_space( $gp ) && strlen( $gp ) > 0 ) : ?>
+				<!-- google+ -->
+				<a href="<?php echo $gp; ?>" rel="author" target="_blank"><span class="genericon genericon-googleplus-alt"></span></a>
 			<?php endif; ?>
-			<?php if(!ctype_space($li) && strlen($li) > 0) : ?>
-			<!-- linkedin -->
-			<a href="<?php echo $li; ?>" target="_blank"><span class="genericon genericon-linkedin"></span></a>
+			
+			<?php if ( ! ctype_space( $li ) && strlen( $li ) > 0 ) : ?>
+				<!-- linkedin -->
+				<a href="<?php echo $li; ?>" target="_blank"><span class="genericon genericon-linkedin"></span></a>
 			<?php endif; ?>
-			<?php if(!ctype_space($rd) && strlen($rd) > 0) : ?>
-			<!-- reddit -->
-			<a href="<?php echo $rd; ?>" target="_blank"><span class="genericon genericon-reddit"></span></a>
+			
+			<?php if ( ! ctype_space( $rd ) && strlen( $rd ) > 0 ) : ?>
+				<!-- reddit -->
+				<a href="<?php echo $rd; ?>" target="_blank"><span class="genericon genericon-reddit"></span></a>
 			<?php endif; ?>
-			<?php if(!ctype_space($su) && strlen($su) > 0) : ?>
-			<!-- stumbleupon -->
-			<a href="<?php echo $su; ?>" target="_blank"><span class="genericon genericon-stumbleupon"></span></a>
+			
+			<?php if ( ! ctype_space( $su ) && strlen( $su ) > 0 ) : ?>
+				<!-- stumbleupon -->
+				<a href="<?php echo $su; ?>" target="_blank"><span class="genericon genericon-stumbleupon"></span></a>
 			<?php endif; ?>
-			<?php if(!ctype_space($fl) && strlen($fl) > 0) : ?>
-			<!-- flickr -->
-			<a href="<?php echo $fl; ?>" target="_blank"><span class="genericon genericon-flickr"></span></a>
+			
+			<?php if ( ! ctype_space( $fl ) && strlen( $fl ) > 0 ) : ?>
+				<!-- flickr -->
+				<a href="<?php echo $fl; ?>" target="_blank"><span class="genericon genericon-flickr"></span></a>
 			<?php endif; ?>
-			<?php if(!ctype_space($rs) && strlen($rs) > 0) : ?>
-			<!-- rss -->
-			<a href="<?php echo $rs; ?>" target="_blank"><span class="genericon genericon-feed"></span></a>
+			
+			<?php if ( ! ctype_space( $rs ) && strlen( $rs ) > 0 ) : ?>
+				<!-- rss -->
+				<a href="<?php echo $rs; ?>" target="_blank"><span class="genericon genericon-feed"></span></a>
 			<?php endif; ?>
 		</div>
 <?php
